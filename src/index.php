46f16aea7fcf4d8ecab36bfb529eee45c7a09a58
Eric Wennerberg <ericwenn@gmail.com>
2016-04-11 13:52:43 +0200

    Compser commit

--- src/index.php
+++ src/index.php
@@ -1,11 +1,4 @@
 <?php
     namespace LocalFeud;
-    use Slim\App;
-    use Slim\Container;
 
-    require_once "../vendor/autoload.php";
-
-    $container = new Container;
-    $app = new App($container);
-
-
+    phpinfo();
