d77ecd21fd1b07797bea3ed4c4fee4deafe885a2
Eric Wennerberg <ericwenn@gmail.com>
2016-04-12 15:16:59 +0200

    Comments moved from single-posts to posts-comments

--- src/endpoints/posts/single.php
+++ src/endpoints/posts/single.php
@@ -50,34 +50,8 @@ $app->get('/posts/{id}/', function( $req, $res, $args) {
         ),
         'date_posted' => $date->format('c'),
         'is_deleted' => false,
-        'comments' => array(
-            array(
-                'id' => 1,
-                'user' => array(
-                    'id' => 2,
-                    'firstname' => 'Krune',
-                    'lastname' => 'Nilsson',
-                    'href' => API_ROOT_URL . '/users/2/'
-                ),
-                'content' => "Lorem ipsum-comment yao.",
-                "date_posted" => $date->format('c'),
-                "is_original_poster" => true,
-                "href" => API_ROOT_URL .'/comments/1/'
-            ),
-            array(
-                'id' => 2,
-                'user' => array(
-                    'id' => 3,
-                    'firstname' => 'Core',
-                    'lastname' => 'Tur',
-                    'href' => API_ROOT_URL . '/users/3/'
-                ),
-                'content' => 'Lorem ipsum-comment 2.',
-                'date_posted' => $date->format('c'),
-                'is_original_poster' => false,
-                'href' => API_ROOT_URL . '/comments/2/'
-            )
-        )
+        'number_of_comments' => 5,
+        'number_of_likes' => 10
     );
 
 
