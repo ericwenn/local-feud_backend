9be19346ac7e49cbf37d552ba89c38841d1d6c30
Eric Wennerberg <ericwenn@gmail.com>
2016-04-19 10:13:17 +0200

    Like and dislike endpoints

--- src/endpoints/posts/single-likes.php
+++ src/endpoints/posts/single-likes.php
@@ -20,90 +20,173 @@ use LocalFeud\Helpers\NameGenerator;
 
 $app->get('/posts/{id}/likes/', function($req, $res, $args) {
 
-        /** @var \Pixie\QueryBuilder\QueryBuilderHandler $qb */
-        $qb = $this->querybuilder;
-        NameGenerator::setQB( $qb );
+    /** @var \Pixie\QueryBuilder\QueryBuilderHandler $qb */
+    $qb = $this->querybuilder;
+    NameGenerator::setQB( $qb );
 
 
-        $likes = $qb->table('likes')->where('likes.postid', '=', $args['id']);
+    $likes = $qb->table('likes')->where('likes.postid', '=', $args['id']);
 
 
-        $likes->leftJoin('post_commentators', function(\Pixie\QueryBuilder\JoinBuilder $table) {
-            $table->on('likes.postid', '=', 'post_commentators.postid');
-            $table->on('likes.userid', '=', 'post_commentators.userid');
-        });
+    $likes->leftJoin('post_commentators', function(\Pixie\QueryBuilder\JoinBuilder $table) {
+        $table->on('likes.postid', '=', 'post_commentators.postid');
+        $table->on('likes.userid', '=', 'post_commentators.userid');
+    });
 
 
-        $likes->leftJoin('posts', 'likes.postid', '=', 'posts.id');
+    $likes->leftJoin('posts', 'likes.postid', '=', 'posts.id');
 
 
-        $likes->select(
-            [
-                'posts.authorid',
-                'likes.userid',
-                'likes.postid',
-                'post_commentators.firstname',
-                'post_commentators.lastname',
-                'likes.date_created'
-            ]
-        );
-
-
-
-        $responseData = $likes->get();
-
-        foreach($responseData as &$like) {
-
-
-            // Format is_original_poster
-            $like->is_original_poster = $like->authorid == $like->userid;
+    $likes->select(
+        [
+            'posts.authorid',
+            'likes.userid',
+            'likes.postid',
+            'post_commentators.firstname',
+            'post_commentators.lastname',
+            'likes.date_created'
+        ]
+    );
 
 
 
+    $responseData = $likes->get();
+
+    foreach($responseData as &$like) {
 
 
-
-            // Format user
-            if( $like->firstname == null && $like->lastname == null) {
-                list($firstname, $lastname) = NameGenerator::generate($like->postid, $like->userid);
-                $like->firstname = $firstname;
-                $like->lastname = $lastname;
-            }
-
-            $user = array(
-                'id' => $like->userid,
-                'firstname' => $like->firstname,
-                'lastname' => $like->lastname,
-                'href' => $this->get('router')->pathFor('user', array(
-                    'id' => $like->userid
-                ))
-            );
-            $like->user = $user;
-
-
-            unset($like->firstname);
-            unset($like->lastname);
-            unset($like->userid);
+        // Format is_original_poster
+        $like->is_original_poster = $like->authorid == $like->userid;
 
 
 
 
 
 
-            // Format date liked
-            $like->date_liked = $like->date_created;
-            unset($like->date_created);
-
-            unset($like->postid);
-
-
+        // Format user
+        if( $like->firstname == null && $like->lastname == null) {
+            list($firstname, $lastname) = NameGenerator::generate($like->postid, $like->userid);
+            $like->firstname = $firstname;
+            $like->lastname = $lastname;
         }
 
-
-
-        $newRes = $res->withJson(
-           $responseData, null, JSON_NUMERIC_CHECK
+        $user = array(
+            'id' => $like->userid,
+            'firstname' => $like->firstname,
+            'lastname' => $like->lastname,
+            'href' => $this->get('router')->pathFor('user', array(
+                'id' => $like->userid
+            ))
         );
+        $like->user = $user;
 
-        return $newRes;
-    })->setName('postLikes');
+
+        unset($like->firstname);
+        unset($like->lastname);
+        unset($like->userid);
+        unset($like->authorid);
+
+
+
+
+
+
+        // Format date liked
+        $like->date_liked = $like->date_created;
+        unset($like->date_created);
+
+        unset($like->postid);
+
+
+    }
+
+
+
+    $newRes = $res->withJson(
+       $responseData, null, JSON_NUMERIC_CHECK
+    );
+
+    return $newRes;
+})->setName('postLikes');
+
+
+
+
+
+
+
+
+
+
+$app->post('/posts/{id}/likes/', function( \Slim\Http\Request $req, \Slim\Http\Response $res, $args ) {
+
+    /** @var \Pixie\QueryBuilder\QueryBuilderHandler $qb */
+    $qb = $this->querybuilder;
+
+    // TODO Authenticated used instead of this
+    $userID = 2;
+    $postID = $args['id'];
+
+
+
+    $likeCheck = $qb->table('likes');
+
+    $likeCheck->where( 'userid', '=', $userID );
+    $likeCheck->where( 'postid', '=', $postID );
+
+    if( $likeCheck->count() === 0 ) {
+
+        $likes = $qb->table('likes');
+        $likes->insert( [
+            'userid' => $userID,
+            'postid' => $postID
+        ]);
+    }
+
+
+    $newRes = $res->withJson(
+        [
+            'status' => 200
+        ], null, JSON_NUMERIC_CHECK
+    );
+
+    return $newRes;
+
+});
+
+
+
+
+
+
+
+$app->delete('/posts/{id}/likes/', function(\Slim\Http\Request $req, \Slim\Http\Response $res, $args) {
+
+
+    /** @var \Pixie\QueryBuilder\QueryBuilderHandler $qb */
+    $qb = $this->querybuilder;
+
+    // TODO Authenticated used instead of this
+    $userID = 2;
+    $postID = $args['id'];
+
+
+    $likes = $qb->table('likes');
+
+    $likes->where( 'userid', '=', $userID );
+    $likes->where( 'postid', '=', $postID );
+
+    $likes->delete();
+
+
+    $newRes = $res->withJson(
+        [
+            'status' => 200
+        ], null, JSON_NUMERIC_CHECK
+    );
+
+    return $newRes;
+
+});
+
+
