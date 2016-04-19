39724116c0c2c247e2fb28ec5417e48d93374bd1
Eric Wennerberg <ericwenn@gmail.com>
2016-04-19 15:40:56 +0200

    Create report endpoint, both for reporting and un-reporting

---
+++ src/endpoints/posts/single-reports.php
@@ -0,0 +1,75 @@
+<?php
+
+
+$app->post('/posts/{id}/reports/', function( \Slim\Http\Request $req, \Slim\Http\Response $res, $args ) {
+
+    /** @var \Pixie\QueryBuilder\QueryBuilderHandler $qb */
+    $qb = $this->querybuilder;
+
+    // TODO Authenticated used instead of this
+    $userID = 2;
+    $postID = $args['id'];
+
+
+
+    $likeCheck = $qb->table('reports');
+
+    $likeCheck->where( 'reporterid', '=', $userID );
+    $likeCheck->where( 'postid', '=', $postID );
+
+    if( $likeCheck->count() === 0 ) {
+
+        $likes = $qb->table('reports');
+        $likes->insert( [
+            'reporterid' => $userID,
+            'postid' => $postID
+        ]);
+    }
+
+
+    $newRes = $res->withJson(
+        [
+            'status' => 200
+        ], null, JSON_NUMERIC_CHECK
+    );
+
+    return $newRes;
+
+});
+
+
+
+
+
+
+
+$app->delete('/posts/{id}/reports/', function(\Slim\Http\Request $req, \Slim\Http\Response $res, $args) {
+
+
+    /** @var \Pixie\QueryBuilder\QueryBuilderHandler $qb */
+    $qb = $this->querybuilder;
+
+    // TODO Authenticated used instead of this
+    $userID = 2;
+    $postID = $args['id'];
+
+
+    $likes = $qb->table('reports');
+
+    $likes->where( 'reporterid', '=', $userID );
+    $likes->where( 'postid', '=', $postID );
+
+    $likes->delete();
+
+
+    $newRes = $res->withJson(
+        [
+            'status' => 200
+        ], null, JSON_NUMERIC_CHECK
+    );
+
+    return $newRes;
+
+});
+
+
